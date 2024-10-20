442f307b12910aa273ac7648e6dc022d1159438b
M303926 <david.kontrovitz@pw.utc.com>
2024-10-20 10:09:50 -0400

    single quotes for continuity

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -3,13 +3,13 @@ import Exo2 from '../fonts/Exo2-Regular.woff2'
 
 const GlobalStyles = createGlobalStyle`
   @font-face {
-    font-family: "${({ theme }) => theme.font}";
+    font-family: '${({ theme }) => theme.font}';
     src: url('${Exo2}') format('woff2');
   }
 
   body {
     margin: 0;
-    font-family: "${({ theme }) => theme.font}", sans-serif;
+    font-family: '${({ theme }) => theme.font}', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
