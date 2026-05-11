8cced01382f39488c5d75069f84148ad0f577e5f
daveKontro <dkontro@hotmail.com>
2026-05-11 07:12:51 -0400

    switch from styled-components to emotion for css-in-js

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,19 +1,17 @@
 import { transparentize } from 'polished'
 
+const palette = {
+  background: '#454145',
+  primary: '#cec2eb',
+}
+
 const theme = {
   font: 'exo2',
   colors: {
-    palette: {
-      background: '#454145',
-      primary: '#cec2eb',
-    },
-    get link() {
-      const link = this.palette.primary
-
-      return {
-        regular: transparentize(0.4, link),
-        hover: transparentize(0.6, link),
-      }
+    palette,
+    link: {
+      main: transparentize(0.4, palette.primary),
+      hover: transparentize(0.6, palette.primary),
     },
   },
 }
